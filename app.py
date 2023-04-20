ae26ddbef953311b4487b596e1c48e3694c55a7d
keitakn <keita.koga.work@gmail.com>
2023-04-21 00:13:52 +0900

    :shirt: #7 Formatterの修正を適応

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ from langchain.chains.conversation.memory import ConversationBufferWindowMemory
 
 # 環境変数にAPIキーを設定
 import os
+
 OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
+
 
 def create_conversational_chain():
     llm = OpenAIChat(model_name="gpt-3.5-turbo", openai_api_key=OPENAI_API_KEY)
@@ -26,6 +28,7 @@ def create_conversational_chain():
 
     return llm_chain
 
+
 SLACK_BOT_TOKEN = os.environ["SLACK_BOT_TOKEN"]
 SLACK_SIGNING_SECRET = os.environ["SLACK_SIGNING_SECRET"]
 
@@ -40,6 +43,7 @@ slack_request_handler = SlackRequestHandler(slack_app)
 
 chain = create_conversational_chain()
 
+
 # メッセージイベントのリスナーを設定
 @slack_app.event("app_mention")
 def command_handler(body, say):
@@ -49,10 +53,12 @@ def command_handler(body, say):
     # Slackに返答を送信
     say(text=chain.predict(input=text), thread_ts=thread_ts)
 
+
 # Slackイベントのエンドポイント
 @app.route("/slack/events", methods=["POST"])
 def slack_events():
     return slack_request_handler.handle(request)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
